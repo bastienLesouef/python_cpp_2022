88ee3c4f4c87d9084b7f581edf24ac2fd5b7b478
bastienLesouef <103256971+bastienLesouef@users.noreply.github.com>
2022-05-05 19:38:12 +0200

    create addNumber function

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -10,13 +10,17 @@ char const* greet() {
   return "hello, world";
 }
 
+double const addNumber(double d1, double d2){
+	return d1 + d2;
+}
+
 namespace py = pybind11;
 
 
 PYBIND11_MODULE(hello_component,greetings)
 {
   greetings.doc() = "greeting_object 1.0";
-  
+  greetings.def("addNumber", &addNumber, "a function that add two double");
   greetings.def("greet", &greet, "a function saying hello");
   greetings.def("getVersion", &getVersion, "a function returning the version");
 }
